e525016f7a059a80edcdb8b7984a791103766ffa
ikechukwu kevin-chika <101973303+Keviinscript@users.noreply.github.com>
2022-08-14 11:02:41 +0100

    second commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    colors: {
+      lightgreen:'#aecd31'
+    },
+    extend: {
+      fontFamily: {
+        'poppins': ['Poppins','sans-serif' ],
+      },
+    },
+  },
+  plugins: [],
+}
